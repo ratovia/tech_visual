97ca86f9df06be8c3ef4d1eba5d6636bb5df289a
Kazushi Yamanishi <keironia187149@gmail.com>
2019-11-26 22:08:13 +0900

    add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :role }
+  it { is_expected.to define_enum_for(:role) }
 end
