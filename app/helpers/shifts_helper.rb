d7f3833bed36a4a8555dadd0c3f22e770e0a85bb
ratovia <ratovia@hotmail.co.jp>
2019-12-28 11:47:04 +0900

    render create view

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,2 +1,14 @@
 module ShiftsHelper
+  DATE_TIME = 24
+  def shift_time_to_array(shift)
+    array = [false] * DATE_TIME
+    array.each_with_index do |_ary, i|
+      array[i] = true if i >= str2hour(shift[:shift_in_at]) && i < str2hour(shift[:shift_out_at])
+    end
+    array
+  end
+
+  def str2hour(time)
+    Time.zone.parse(time.to_s).strftime("%H").to_i
+  end
 end
