886f5e10a9342b538f235f9957758f1ec03dc3a8
Kazushi Yamanishi <keironia187149@gmail.com>
2019-12-28 21:51:13 +0900

    add shift generate button to shifts#index for admin

--- app/admin/shifts.rb
+++ app/admin/shifts.rb
@@ -6,6 +6,11 @@ ActiveAdmin.register Shift do
   filter :shift_in_at
   filter :shift_out_at
 
+  # title_barの右側に新しいアクションを追加する
+  action_item :shift_generate, only: :index do
+    link_to "シフト作成", api_shift_generator_path, method: :post if current_user.admin?
+  end
+
   controller do
     def index
       redirect_to new_user_session_path and return unless user_signed_in?
