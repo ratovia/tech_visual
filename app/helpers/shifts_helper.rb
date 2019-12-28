53e60cd00a7ae402d1076d5f42c54a7416a61ab0
ratovia <ratovia@hotmail.co.jp>
2019-12-28 20:48:27 +0900

    constant load form setting.yml

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,7 +1,6 @@
 module ShiftsHelper
-  DATE_TIME = 24
   def shift_time_to_array(shift)
-    array = [false] * DATE_TIME
+    array = [false] * Settings.DATE_TIME
     array.each_with_index do |_ary, i|
       array[i] = true if i >= str2hour(shift[:shift_in_at]) && i < str2hour(shift[:shift_out_at])
     end
