0f527d8d8e34a640d431eca682432193acc2691a
ratovia <ratovia@hotmail.co.jp>
2020-01-11 14:19:41 +0900

    extend seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,53 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-require 'csv'
-
-# users
-CSV.foreach('db/csv/users.csv', headers: true) do |row|
-  User.where(email: row['email']).first_or_create do |user|
-    user.name = row['name']
-    user.email = row['email']
-    user.password = row['password']
-    user.role = row['role'].to_i
+start = DateTime.new(2020, 1, 1)
+finish = DateTime.new(2020, 1, 10)
+# Admin User
+if Rails.env.development?
+  User.where(email: 'admin@namba').first_or_create do |user|
+    user.name = 'シフト職人'
+    user.password = 'adminpass'
+    user.password_confirmation = 'adminpass'
+    user.role = 1
   end
 end
+# Normal User
+users = []
+attendances = []
+50.times do |i|
+  user = User.create(
+    name: Faker::Name.last_name,
+    email: Faker::Internet.free_email,
+    password: Faker::Internet.password(min_length: 8)
+  )
 
-# work_roles
-CSV.foreach('db/csv/work_roles.csv', headers: true) do |row|
-  WorkRole.where(name: row['name']).first_or_create
-end
-
-# attendances
-CSV.foreach('db/csv/attendances.csv', headers: true) do |row|
-  Attendance.where(date: row['date'], user_id: row['user_id']).first_or_create do |attendance|
-    attendance.attendance_at = row['attendance_at']
-    attendance.leaving_at = row['leaving_at']
+  time_array = [rand(23),rand(23)].sort
+  (start..finish).each do |this_day|
+    attendances << Attendance.new(date: this_day, attendance_at: time_array[0], leaving_at: time_array[1],user_id: user.id)
   end
 end
+Attendance.import! attendances
 
-# required_resources
-CSV.foreach('db/csv/required_resources.csv', headers: true) do |row|
-  RequiredResource.where(
-    what_day: row['what_day'],
-    clock_at: row['clock_at'],
-    work_role_id: row['work_role_id']
-  ).first_or_create do |wr|
-    wr.count = row['count']
+requiredresources = []
+4.times do |n|
+  workrole = WorkRole.create!(name: Faker::Company.name)
+  req = ([0] * 23).map{ |x| rand(10) }.sort.rotate(rand(23)) #0時〜23時
+  req.each_with_index do |data, i|
+    (start..finish).each do |this_day|
+      requiredresources << RequiredResource.new(what_day: RequiredResource.on_(this_day), clock_at: i, count: data ,work_role_id: workrole.id)
+    end
   end
 end
-
-# shifts
-CSV.foreach('db/csv/shifts.csv', headers: true) do |row|
-  Shift.where(
-    shift_in_at: row['shift_in_at'],
-    shift_out_at: row['shift_out_at'],
-    user_id: row['user_id'],
-    work_role_id: row['work_role_id']
-  ).first_or_create
-end
+RequiredResource.import! requiredresources
