fb1f587261b0bf0e54323f0c595a10ce79cd21b8
ratovia <ratovia@hotmail.co.jp>
2020-01-15 00:53:48 +0900

    remove XXX code

--- app/assets/javascripts/sum_resource_color.js
+++ app/assets/javascripts/sum_resource_color.js
@@ -4,9 +4,8 @@ $(function(){
     const workrole_ids = $("[data-workrole-ids]").data("workrole-ids")
     workrole_ids.forEach(function(id){
       for(let num = 8;num < 24;num++){
-        // XXX: num=8の時、data-time=9のデータが取れてしまう。
-        sum = $(`[data-resource-type=sum]+[data-time=${num - 1}]+[data-workrole-id=${id}]`);
-        req = $(`[data-resource-type=req]+[data-time=${num - 1}]+[data-workrole-id=${id}]`);
+        sum = $(`[data-resource-type=sum][data-time=${num}][data-workrole-id=${id}]`);
+        req = $(`[data-resource-type=req][data-time=${num}][data-workrole-id=${id}]`);
 
         if(parseInt(req[0].innerText) > parseInt(sum[0].innerText)){
           sum.addClass("not_enough_resource");
