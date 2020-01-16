892c4b5f1f77faaa4a3f077cbc07625cfd3003dd
ratovia <ratovia@hotmail.co.jp>
2020-01-16 12:16:30 +0900

    update for multiple date with slick

--- app/assets/javascripts/sum_resource_color.js
+++ app/assets/javascripts/sum_resource_color.js
@@ -2,15 +2,17 @@ $(function(){
   $(".shift-tables").ready(function(){
     // TODO: slickの複数日付にも対応する
     const workrole_ids = $("[data-workrole-ids]").data("workrole-ids")
-    workrole_ids.forEach(function(id){
-      for(let num = 8;num < 24;num++){
-        sum = $(`[data-resource-type=sum][data-time=${num}][data-workrole-id=${id}]`);
-        req = $(`[data-resource-type=req][data-time=${num}][data-workrole-id=${id}]`);
-
-        if(parseInt(req[0].innerText) > parseInt(sum[0].innerText)){
-          sum.addClass("not_enough_resource");
+    $("[data-date]").each(function(){
+      let _this = this;
+      workrole_ids.forEach(function(id){
+        for(let num = 8;num < 24;num++){
+          sum = $(_this).find(`[data-resource-type=sum][data-time=${num}][data-workrole-id=${id}]`);
+          req = $(_this).find(`[data-resource-type=req][data-time=${num}][data-workrole-id=${id}]`);
+          if(parseInt(req[0].innerText) > parseInt(sum[0].innerText)){
+            sum.addClass("not_enough_resource");
+          }
         }
-      }
+      })
     })
   });
 })
