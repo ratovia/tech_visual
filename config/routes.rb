1403d31d27916496ede566619b3df8a25592b256
Kazushi Yamanishi <keironia187149@gmail.com>
2020-01-16 19:13:53 +0900

    change shift_generator#create method POST to GET

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   namespace :api do
-    post 'shift_generator' => 'shift_generator#create'
+    resource :shift_generator, only: :show
   end
 end
