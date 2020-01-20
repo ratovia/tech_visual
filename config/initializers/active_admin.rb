a5fa9fc3212e5939592c93b5784509cefc08feb3
Kazushi Yamanishi <keironia187149@gmail.com>
2020-01-20 22:12:07 +0900

    add application.js to active_admin.js

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -217,7 +217,8 @@ ActiveAdmin.setup do |config|
   #   config.register_stylesheet 'my_print_stylesheet.css', media: :print
   #
   # To load a javascript file:
-  #   config.register_javascript 'my_javascript.js'
+    config.register_javascript 'https://code.jquery.com/jquery-3.4.1.min.js'
+    config.register_javascript 'application.js'
 
   # == CSV options
   #
