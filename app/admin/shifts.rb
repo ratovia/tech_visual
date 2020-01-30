535dbdfcc9916e0ca15a1f0398db1138f0257836
Kazushi Yamanishi <keironia187149@gmail.com>
2020-01-30 14:48:49 +0900

    create this_month days array

--- app/admin/shifts.rb
+++ app/admin/shifts.rb
@@ -2,13 +2,16 @@ ActiveAdmin.register Shift do
   permit_params :work_role_id, :user_id, :shift_in_at, :shift_out_at
 
   filter :user
-  filter :work_role
-  filter :shift_in_at
-  filter :shift_out_at
 
   controller do
     def index
       redirect_to new_user_session_path and return unless user_signed_in?
+      first_day = DateTime.now.beginning_of_month
+      @days = []
+      DateTime.now.end_of_month.day.times do |day|
+        @days << first_day
+        first_day += 1.days
+      end
       super
     end
   end
