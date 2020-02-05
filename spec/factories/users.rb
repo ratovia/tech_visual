fba818aa1644112b2e355fcb9f3879b7562e7191
Kazushi Yamanishi <keironia187149@gmail.com>
2020-02-05 17:20:26 +0900

    add method test for Shift.build_from_genoms

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { Devise.friendly_token }
+    role { 'employee' }
   end
 end
--- spec/models/shift_spec.rb
+++ spec/models/shift_spec.rb
@@ -1,8 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Shift, type: :model do
-  it { is_expected.to belong_to(:user) }
-  it { is_expected.to belong_to(:work_role) }
-  it { is_expected.to validate_presence_of :shift_in_at }
-  it { is_expected.to validate_presence_of :shift_out_at }
+  describe 'validation' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:work_role) }
+    it { is_expected.to validate_presence_of :shift_in_at }
+    it { is_expected.to validate_presence_of :shift_out_at }
+  end
+
+  describe 'methods' do
+    context 'self.build_from_genoms' do
+      let(:user) { create(:user) }
+      let(:work_roles) { create_list(:work_role, 4) }
+      let(:genoms) {{
+        this_day: "Wed, 01 Jan 2020 00:00:00 +0000",
+        shifts: [
+          user_id: user.id,
+          array: [nil, nil, nil, nil, nil, nil, 3, 2, 2, 2, 1, 2, 4, 0, 1, 1, nil, nil, nil, nil, nil, nil, nil, nil]
+        ]
+      }}
+
+      it '想定しているシフトの件数(6件)と一致している' do
+        shifts = Shift.build_from_genoms(genoms)
+        expect(shifts.length).to eq 6
+      end
+
+      it '最初のシフトが想定通りの値になっている' do
+        shifts = Shift.build_from_genoms(genoms)
+        valid_shift_attributes = Shift.new(
+          user_id: user.id,
+          work_role_id: 3,
+          shift_in_at: DateTime.new(2020, 1, 1, 6),
+          shift_out_at: DateTime.new(2020, 1, 1, 7)
+        ).attributes
+        expect(shifts[0].attributes).to eq valid_shift_attributes
+      end
+
+      it '2番目のシフトが想定通りの値になっている' do
+        shifts = Shift.build_from_genoms(genoms)
+        valid_shift_attributes = Shift.new(
+          user_id: user.id,
+          work_role_id: 2,
+          shift_in_at: DateTime.new(2020, 1, 1, 7),
+          shift_out_at: DateTime.new(2020, 1, 1, 10)
+        ).attributes
+        expect(shifts[1].attributes).to eq valid_shift_attributes
+      end
+
+      it '最後のシフトが想定通りの値になっている' do
+        shifts = Shift.build_from_genoms(genoms)
+        valid_shift_attributes = Shift.new(
+          user_id: user.id,
+          work_role_id: 1,
+          shift_in_at: DateTime.new(2020, 1, 1, 14),
+          shift_out_at: DateTime.new(2020, 1, 1, 16)
+        ).attributes
+        expect(shifts[-1].attributes).to eq valid_shift_attributes
+      end
+
+    end
+  end
 end
