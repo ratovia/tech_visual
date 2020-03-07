19bbb5fe075ffae66e56646119ea95b375bda60c
Kazushi Yamanishi <keironia187149@gmail.com>
2020-03-07 14:23:38 +0900

    post user_genom_parametor to shifts_update

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -5,7 +5,17 @@ class ShiftsController < ApplicationController
     redirect_to admin_shifts_path unless current_user.admin?
     @workroles = WorkRole.all
     # @days = (Date.current.beginning_of_month..Date.current.end_of_month).to_a
-    @days = (Date.new(2020,2,1)..Date.new(2020,2,2)).to_a
+    @days = (Date.new(2020,2,1)..Date.new(2020,2,1)).to_a
     @users = User.all
+  end
+
+  def update
+    binding.pry
+  end
+
+  private
+
+  def shift_params
+    params.permit(:day, :user_id, :shift_array)
   end
 end
